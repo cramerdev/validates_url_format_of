ddec4f4b9db4d35fc2ce09aca988c15698624a64
Nathan L Smith <nlloyds@gmail.com>
2009-07-08 14:39:45 -0500

    Added domain name validation

--- init.rb
+++ init.rb
@@ -26,6 +26,25 @@ module ValidatesUrlFormatOf
     end
   end
   
+  # Domain name only validation, added by smith on 6/10/09
+  DOMAIN_NAME_REGEXP = %r{
+    \A
+    ( (xn--)?[^\W_]+([-.][^\W_]+)*\.[a-z]{2,6}\.? |  # domain (including Punycode/IDN)...
+        #{IPv4_PART}(\.#{IPv4_PART}){3} )            # or IPv4
+    \Z
+  }iux
+
+  def validates_domain_name_format_of(*attr_names)
+    options = { :allow_nil => false,
+                :allow_blank => false,
+                :with => DOMAIN_NAME_REGEXP }                
+    options = options.merge(attr_names.pop) if attr_names.last.is_a?(Hash)
+
+    attr_names.each do |attr_name|
+      message = attr_name.to_s.match(/(_|\b)URL(_|\b)/i) ? DEFAULT_MESSAGE_URL : DEFAULT_MESSAGE
+      validates_format_of(attr_name, { :message => message }.merge(options))
+    end
+  end
 end
 
 ActiveRecord::Base.extend(ValidatesUrlFormatOf)
